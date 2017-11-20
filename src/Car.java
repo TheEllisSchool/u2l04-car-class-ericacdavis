1a68627a948a03cbae9d1f22c813372def13a14c
ericadavis <ericadavis@ericas-air.fios-router.home>
2017-11-20 11:00:24 -0500

    Done!

--- src/Car.java
+++ src/Car.java
@@ -1,15 +1,57 @@
 
 public class Car {
 	//------attributes----//
+	private int yearModel;
+	private String make;
+	private int speed=0;
 	
 	//------methods------//
 	//constructor
+	public Car (int ym, String m, int s) {
+		yearModel = ym;
+		make= m;
+		speed= s;
+	}
 	
 	//setters
-	
+	public void setSpeed(int s) {
+			if (s>=-150 && s<=150) {
+				speed = s;
+				return;
+			}
+			else {
+				System.out.println("Speed not possible");
+			}
+	}
 	//getters
-	
+	public int getyearModel() {
+		return yearModel;
+	}
+	public String make() {
+		return make;
+	}
+	public int speed() {
+		return speed;
+	}
+	//string
+	public String toString() {
+		String Car = "Year Model " + yearModel + "\n" + "Make: " + make + "\n" + "Speed: "+ speed;
+		return Car;
+	}
 	//accelerate
+	public int accelerate () {
+		if (speed<=145){
+		speed+=5;
+		}
+		return speed;
+		}
 	
 	//brake
+	public int breaks () {
+		if(speed>=-150){
+			speed-=5;
+		}
+		return speed;
+	
+	}
 }
--- src/CarDemo.java
+++ src/CarDemo.java
@@ -3,11 +3,18 @@ public class CarDemo {
 
 	public static void main(String[] args) {
 		// create a car object
-		
+		Car convertable = new Car (2001, "porsche", 140);
+		System.out.println(convertable);
 		//call accelerate 5 times (use a loop)
-		
+		for (int i=0; i<=5; i++) {
+			convertable.accelerate();
+			System.out.println("speed after acceleration is: " + convertable.speed());
+		}
 		//call brake 5 times (use a loop)
-
+		for (int i=0; i<=5; i++) {
+			convertable.breaks();
+			System.out.println("speed after breaking is: " + convertable.speed());
+		}
 	}
 
 }
